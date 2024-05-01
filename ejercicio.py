87333b6058e01d0ea0b408252e1ed91517dab115
Edgar Moreano <moreanoe@gmail.com>
2024-04-30 23:07:20 -0500

    Lista de Compras

--- ejercicio.py
+++ ejercicio.py
@@ -7,4 +7,8 @@ print(f"Los productos en la lista de compras son: {productos}")
 
 # Convertir la lista de compras en una tupla
 def convertir_lista_a_tupla(lista):
-    return # Utiliza una función para convertir 
+    tupla = tuple(lista)
+    return tupla
+
+if __name__ == "__main__":
+    convertir_lista_a_tupla(productos)
